70732f06a91e3af1fd71ee7ce61f741f76991c28
Luã Maury Maquiné da Silva <lmmds.snf20@uea.edu.br>
2021-06-05 11:55:33 -0400

    New game add

---
+++ New Game Luã Maquiné/Assets/LevelControler.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelControler : MonoBehaviour
+{
+    private static int _nextLevelIndex = 1;
+    private Enemy[] _enemies;
+
+    private void OnEnable()
+    {
+        _enemies = FindObjectsOfType<Enemy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        foreach(Enemy enemy in _enemies)
+        {
+            if (enemy != null)
+                return;
+        }
+
+        Debug.Log("You Killed all enemies");
+
+        _nextLevelIndex++;
+        string nextLevelName = "Level" + _nextLevelIndex;
+        SceneManager.LoadScene(nextLevelName);
+    }
+}
